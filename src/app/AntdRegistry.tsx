24ef89b68a0ec07089046deeafdfc5dfaa54cf5b
hyper-richi <kamalov.job@gmail.com>
2025-11-15 14:39:07 +0300

    finish

--- src/app/AntdRegistry.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { ConfigProvider } from "antd";
-import React from "react";
-
-export default function AntdRegistry({ children }: { children: React.ReactNode }) {
-  return <ConfigProvider>{children}</ConfigProvider>;
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
-import AntdRegistry from "./AntdRegistry";
-import StyledJsxRegistry from "./registry";
-import { StyleProvider, createCache } from "@ant-design/cssinjs";
 import "antd/dist/reset.css";
 import "./globals.scss";
 
@@ -28,11 +25,6 @@ const GeistMono = localFont({
   variable: "--font-geist-mono",
 });
 
-// const cache = createCache(); // создаём кэш
-
-// export const revalidate = 0;
-// export const fetchCache = "force-no-store";
-// export const dynamic = "force-static";
 
 export const metadata: Metadata = {
   title: "GitHub Users",
